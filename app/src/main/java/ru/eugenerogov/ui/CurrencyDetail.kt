bd4d6ef6f9c55c147a5d9e9d0773c554178b87d8
Eugene Rogov <digest2003@mail.ru>
2021-01-22 14:28:45 +0300

    added repo

---
+++ app/src/main/java/ru/eugenerogov/ui/CurrencyDetail.kt
@@ -0,0 +1,16 @@
+package ru.eugenerogov.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.eugenerogov.R
+
+class CurrencyDetail : Fragment(R.layout.currency_detail_fragment) {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
